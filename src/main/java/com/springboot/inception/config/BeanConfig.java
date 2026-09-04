c241dd176633e96b672c34694adf47d29424937f
saurabhnakade <saurabh.nakade23@gmail.com>
2026-09-05 01:29:47 +0530

    @Scope annotation to define scope of a bean

--- src/main/java/com/springboot/inception/config/BeanConfig.java
+++ src/main/java/com/springboot/inception/config/BeanConfig.java
@@ -3,11 +3,13 @@ package com.springboot.inception.config;
 import com.springboot.inception.payment.PaymentService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class BeanConfig {
 
     @Bean
+    @Scope("singleton")
     // This holds priority over @Component annotation in PaymentService class
     public PaymentService paymentService() {
         return new PaymentService();
