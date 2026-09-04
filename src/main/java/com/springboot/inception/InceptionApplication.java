a53189228b08f1af4a1737e24cb2908272cfc361
saurabhnakade <saurabh.nakade23@gmail.com>
2026-09-05 01:02:13 +0530

    spring boot project setup

---
+++ src/main/java/com/springboot/inception/InceptionApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.inception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InceptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InceptionApplication.class, args);
+	}
+
+}
