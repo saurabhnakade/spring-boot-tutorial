d6594f43221658b6ea4dce4489ef43b358e4f4fb
saurabhnakade <saurabh.nakade23@gmail.com>
2026-09-05 01:48:09 +0530

    using @ConditionalOnProperty to configure beans using properties

--- src/main/java/com/springboot/inception/notification/impl/EmailNotification.java
+++ src/main/java/com/springboot/inception/notification/impl/EmailNotification.java
@@ -2,10 +2,12 @@ package com.springboot.inception.notification.impl;
 
 import com.springboot.inception.notification.NotificationService;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 @Component
 @Qualifier("emailNotification")
+@ConditionalOnProperty(name = "notification.email", havingValue = "true")
 public class EmailNotification implements NotificationService {
 
     @Override
--- src/main/java/com/springboot/inception/notification/impl/SmsNotification.java
+++ src/main/java/com/springboot/inception/notification/impl/SmsNotification.java
@@ -2,10 +2,12 @@ package com.springboot.inception.notification.impl;
 
 import com.springboot.inception.notification.NotificationService;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 @Component
 @Qualifier("smsNotification")
+@ConditionalOnProperty(name = "notification.sms", havingValue = "true")
 public class SmsNotification implements NotificationService {
 
     @Override
