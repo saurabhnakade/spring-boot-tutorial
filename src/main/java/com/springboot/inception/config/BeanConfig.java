ff3d733d0f9b9cea45131986ed224b479eacb11f
saurabhnakade <saurabh.nakade23@gmail.com>
2026-09-05 01:54:58 +0530

    collection dependency injection

--- src/main/java/com/springboot/inception/config/BeanConfig.java
+++ src/main/java/com/springboot/inception/config/BeanConfig.java
@@ -9,15 +9,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
+import java.util.Map;
+
 @Configuration
 public class BeanConfig {
 
     private final EmailNotification emailNotification;
     private final SmsNotification smsNotification;
+    private final Map<String, NotificationService> allNotificationServices;
 
-    BeanConfig(EmailNotification emailNotification, SmsNotification smsNotification) {
+    BeanConfig(EmailNotification emailNotification, SmsNotification smsNotification, Map<String, NotificationService> allNotificationServices) {
         this.emailNotification = emailNotification;
         this.smsNotification = smsNotification;
+        this.allNotificationServices = allNotificationServices;
     }
 
     @Bean
